8757945f3559c3d74fc43a55a87342e9896b5176
Jason McGloin <jason.mcgloin@gmail.com>
2019-09-16 10:19:58 -0400

    heroku bug fix attempt

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,10 @@ Bundler.require
 
 require 'sinatra/flash'
 
-configure do
-	mime_type :javascript, 'text/javascript'
-	mime_type :css, 'text/css'
-end
+# configure do
+# 	mime_type :javascript, 'text/javascript'
+# 	mime_type :css, 'text/css'
+# end
 
 require_all 'app'
 require_all 'public'
